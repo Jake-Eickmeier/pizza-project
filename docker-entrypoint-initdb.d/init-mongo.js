723833fc11d3aa24569fde20a199765dbc611850
Jake <jake.eickmeier@gmail.com>
2025-08-18 14:06:16 -0400

    Adding data for new pizza specials collection

--- docker-entrypoint-initdb.d/init-mongo.js
+++ docker-entrypoint-initdb.d/init-mongo.js
@@ -2,17 +2,23 @@
 db = new Mongo().getDB("test");
 
 
-// Collection and data for pizza menu service
+// Collections and data for pizza menu service
 db.createCollection('pizzaMenu', { capped: false });
+db.createCollection('pizzaSpecials', { capped: false });
 
-db.pizzaMenu.insert([
-    {"name": "pepperoni", "toppings": ["cheese", "pepperoni", "sauce"]},
-    {"name": "cheese", "toppings": ["cheese"] }
+db.pizzaMenu.insertMany([
+    {"_id": ObjectId("68715674e36ec57a5dba8a7d"), "name": "pepperoni", "toppings": ["cheese", "pepperoni", "sauce"], "priceInCents": 1399, "tags": ["MEATLOVER"]},
+    {"_id": ObjectId("68715674e36ec57a5dba8a7e"), "name": "cheese", "toppings": ["cheese"], "priceInCents": 1299, "tags": ["VEGETARIAN"]}
+]);
+
+db.pizzaSpecials.insertMany([
+    {"pizzaId": ObjectId("68715674e36ec57a5dba8a7d"), "newPriceInCents": 999, "specialDay": "THURSDAY", "expirationDate": null },
+    {"pizzaId": ObjectId("68715674e36ec57a5dba8a7e"), "newPriceInCents": 899, "specialDay": "FRIDAY", "expirationDate": null }
 ]);
 
 // Collection and data for pizza ordering service
 db.createCollection('pizzaOrders', { capped: false });
-db.pizzaOrders.insert([
+db.pizzaOrders.insertMany([
     {"itemsPurchased": {"pepperoni": 1, "cheese": 2}, "userEmailAddress": "pizzaLover1@gmail.com", "purchaseDate": new Date(), "priceInCents": 2699},
     {"itemsPurchased": {"pepperoni": 3, "cheese": 1}, "userEmailAddress": "pizzaEnjoyer7@gmail.com", "purchaseDate": new Date(), "priceInCents": 3799}
 ]);
